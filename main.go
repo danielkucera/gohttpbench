c79b860815d3ad754253cf4ea3ed3080df45b133
Daniel Kucera <daniel.kucera@gmail.com>
2017-09-14 16:06:40 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+import "strconv"
+import "net/http"
+import "os"
+import "time"
+
+func main(){
+
+	url := os.Args[1]
+	threads,_ := strconv.Atoi(os.Args[2])
+	status,_ := strconv.Atoi(os.Args[3])
+
+	cnt := 0
+	lastcnt := 0
+	
+	for i := 0; i < threads; i++ {
+		go func(){
+			for {
+				resp, err := http.Get(url)
+				if err == nil {
+					if resp.StatusCode == status {
+						cnt += i
+					} else {
+						fmt.Printf("unexpected status: %d\n", resp.Status)
+					}
+					resp.Body.Close()
+				} else {
+					fmt.Printf("request error: %s\n", err)
+				}
+			}
+		}()
+	}
+	
+	for {
+		time.Sleep(time.Second)
+		diff := cnt-lastcnt
+		fmt.Printf("resp/sec: %d\n", diff)
+		lastcnt = cnt
+	}
+
+}
